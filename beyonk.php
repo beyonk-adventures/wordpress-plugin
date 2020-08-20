eec4079b054cbef3655f775d3e382c9baf903190
Antony Jones <antony@beyonk.com>
2020-08-20 15:27:40 +0100

    bump version

--- beyonk.php
+++ beyonk.php
@@ -3,7 +3,7 @@
   * Plugin Name: Beyonk SDK
   * Plugin URI: https://sdk.beyonk.com/
   * Description: Plugin for integrating with the Beyonk Booking Platform
-  * Version: 0.0.12
+  * Version: 0.0.13
   * Author: Antony MacKenzie-Jones
   * Author URI: http://beyonk.com/
   **/
